9843992ad8441d4b8a963add9cdd975ef39d6168
zhaohonglee <296222105@qq.com>
2020-12-03 15:05:40 +0800

    20201203 afternoon commit

--- src/App.js
+++ src/App.js
@@ -12,23 +12,40 @@ export default class App extends Component {
     loading: false
   }
 
+  // 页面初始加载的时候显示该页面，当进行搜索的时候用搜索结果替换
   async componentDidMount() {
     // console.log('123')
     // 请求数据
     // 测试是否可以获取环境变量
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID)
+    // console.log(process.env.REACT_APP_GITHUB_CLIENT_ID)
 
     this.setState({
       loading: true
     })
+    // 启用git OAuth Apps 将ID和SECRET按照一下格式请求
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    console.log(res.data)
+    // console.log(res.data)
     this.setState({
       users: res.data,
       loading: false
     })
   }
 
+  // 子->父的实现方式，这个是父组件为了传值而设立的方法
+  searchUsers = async text => {
+    console.log(text, '********')
+
+    this.setState({
+      loading: true
+    })
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    // console.log(res.data, '**********')
+    this.setState({
+      users: res.data.items,
+      loading: false
+    })
+  }
 
   render() {
     const test = 'Github FinderV1.0.0'
@@ -36,7 +53,7 @@ export default class App extends Component {
       <div className='App'>
         <Navbar title={test}></Navbar>
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 export default class Search extends Component {
   state = {
     text: ''
+  }
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
   }
 
   onChange = (e) => {
@@ -14,7 +20,10 @@ export default class Search extends Component {
   onSubmit = (e) => {
     // 这样无需绑定this,比较简洁。
     e.preventDefault()
-    console.log(this.state.text)
+    // 打印测试
+    // console.log(this.state.text)
+    // 此处的代码，用到了从子组件向父组件传值的实现方式，子->父（子组件调用父组件中的一个提前定义好的方法来实现）
+    this.props.searchUsers(this.state.text)
   }
 
 
