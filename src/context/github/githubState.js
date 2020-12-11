eac58af7b4cac00da4e041e92fcb496531d2e3ef
zhaohonglee <296222105@qq.com>
2020-12-11 09:29:49 +0800

    last 20201211

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -1,8 +1,28 @@
-import React, { useReducer } from 'react'
+import React, {
+  useReducer
+} from 'react'
 import axios from 'axios'
 import GithubContext from './githubContext'
 import GithubReducer from './githubReducer'
-import { SEARCH_USERS, GET_USER, GET_REPOS, SET_LOADING, CLEAR_USERS } from '../types'
+import {
+  SEARCH_USERS,
+  GET_USER,
+  GET_REPOS,
+  SET_LOADING,
+  CLEAR_USERS
+} from '../types'
+
+let githubClientId;
+let githubClientSecret;
+if (process.env.NODE_ENV !== 'production') {
+  // 使用本地环境变量
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
+} else {
+  // 生产环境变量
+  githubClientId = process.env.GITHUB_CLIENT_ID
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET
+}
 
 const GithubState = props => {
   const initialState = {
@@ -17,7 +37,7 @@ const GithubState = props => {
   const searchUsers = async text => {
     // console.log(text, '********')
     setLoading()
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`)
     // console.log(res.data, '**********')
 
     dispatch({
@@ -30,7 +50,7 @@ const GithubState = props => {
   const getUser = async login => {
     // console.log(login, '********')
     setLoading()
-    const res = await axios.get(`https://api.github.com/users/${login}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    const res = await axios.get(`https://api.github.com/users/${login}?client_id=${githubClientId}&client_secret=${githubClientSecret}`)
     console.log(res.data, '***getUser**')
     dispatch({
       type: GET_USER,
@@ -43,11 +63,11 @@ const GithubState = props => {
   const getUserRepos = async login => {
     // console.log(login, '********')
     setLoading()
-    const res = await axios.get(`https://api.github.com/users/${login}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    const res = await axios.get(`https://api.github.com/users/${login}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`)
 
     dispatch({
-      type:GET_REPOS,
-      payload:res.data
+      type: GET_REPOS,
+      payload: res.data
     })
   }
 
@@ -58,19 +78,23 @@ const GithubState = props => {
 
 
   // set loading
-  const setLoading = () => dispatch({ type: SET_LOADING })
+  const setLoading = () => dispatch({
+    type: SET_LOADING
+  })
 
   return (
-    <GithubContext.Provider value={{
-      users: state.users,
-      user: state.user,
-      repos: state.repos,
-      loading: state.loading,
-      searchUsers,
-      clearUsers,
-      getUser,
-      getUserRepos
-    }} >
+    <GithubContext.Provider value={
+      {
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos
+      }
+    } >
       {props.children}
     </GithubContext.Provider>
   )
